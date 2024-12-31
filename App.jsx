84f1a85167c95a13556b2768c8becc88a1cfdc37
Dimitrije <gonnia119@gmail.com>
2024-12-31 13:15:54 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MovieDashboard from './components/MovieDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <MovieDashboard />
+    </div>
+  );
+}
+
+export default App;
